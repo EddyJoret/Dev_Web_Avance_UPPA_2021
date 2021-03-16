b8495de17a6624d657fb31c7ae2c0c508dcbf2de
EddyJoret <eddyjoret@hotmail.fr>
2021-03-16 18:06:46 +0100

    SQL

---
+++ ejoret001.sql
@@ -0,0 +1,49 @@
+CREATE TABLE JOUEUR
+(
+    Code_Joueur INT, /*PK + FK -> STATISTIQUE*/
+    Pseudo VARCHAR(30),
+    Mdp VARCHAR(40),
+    Age INT,
+    Sexe CHAR(1),
+    Ville VARCHAR(30),
+    Nb_Pts_Actuel INT
+);
+
+CREATE TABLE PARTIE
+(
+    Code_Partie INT, /*PK*/
+    Code_Joueur1 INT, /*FK -> JOUEUR*/
+    Code_Joueur2 INT, /*FK -> JOUEUR*/
+    Code_Joueur3 INT, /*FK -> JOUEUR*/
+    Code_Joueur4 INT, /*FK -> JOUEUR*/
+    Code_Joueur5 INT, /*FK -> JOUEUR*/
+    Code_Joueur6 INT, /*FK -> JOUEUR*/
+    Dernier_L CHAR(3)
+);
+
+CREATE TABLE STATISTIQUE
+(
+    Code_Joueur INT, /*PK*/
+    Nb_Parti INT,
+    Nb_Victoire INT,
+    Nb_Pts_Tot INT,
+    Sc_Moyen INT,
+    Su_Moyen_G INT,
+    CV_Moyen_P INT
+);
+
+drop table JOUEUR;
+drop table PARTIE;
+drop table STATISTIQUE;
+
+ALTER TABLE PARTIE ADD CONSTRAINT PK_Code_Partie PRIMARY KEY (Code_Partie);
+ALTER TABLE JOUEUR ADD CONSTRAINT PK_Code_Joueur PRIMARY KEY (Code_Joueur);
+ALTER TABLE STATISTIQUE ADD CONSTRAINT PK_Code_Joueur_Stat PRIMARY KEY (Code_Joueur);
+
+ALTER TABLE PARTIE ADD CONSTRAINT FK_Code_Joueur FOREIGN KEY(Code_Joueur1) REFERENCES JOUEUR(Code_Joueur);
+ALTER TABLE PARTIE ADD CONSTRAINT FK_Code_Joueur2 FOREIGN KEY(Code_Joueur2) REFERENCES JOUEUR(Code_Joueur);
+ALTER TABLE PARTIE ADD CONSTRAINT FK_Code_Joueur3 FOREIGN KEY(Code_Joueur3) REFERENCES JOUEUR(Code_Joueur);
+ALTER TABLE PARTIE ADD CONSTRAINT FK_Code_Joueur4 FOREIGN KEY(Code_Joueur4) REFERENCES JOUEUR(Code_Joueur);
+ALTER TABLE PARTIE ADD CONSTRAINT FK_Code_Joueur5 FOREIGN KEY(Code_Joueur5) REFERENCES JOUEUR(Code_Joueur);
+ALTER TABLE PARTIE ADD CONSTRAINT FK_Code_Joueur6 FOREIGN KEY(Code_Joueur6) REFERENCES JOUEUR(Code_Joueur);
+ALTER TABLE JOUEUR ADD CONSTRAINT FK_Code_Joueur_Stat FOREIGN KEY(Code_Joueur) REFERENCES STATISTIQUE(Code_Joueur); 
