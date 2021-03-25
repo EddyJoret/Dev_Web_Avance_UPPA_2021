9f8176700aa7494a88513d100b05837c0a46b1b0
EddyJoret <eddyjoret@hotmail.fr>
2021-03-25 17:23:25 +0100

    Update FctResumePartie.java

--- Projet_CulDeChouette_2021/src/FctPojo/FctResumePartie.java
+++ Projet_CulDeChouette_2021/src/FctPojo/FctResumePartie.java
@@ -72,7 +72,7 @@ public class FctResumePartie {
     //fct de calcul du score à faire avant
     
         //Nombre total de fois qu'une valeur de dès précise est tombée
-    public int total_Lance_De(int valeur) throws SQLException{
+    public int total_Nb_Lance_De(int valeur) throws SQLException{
         Statement req = connection.createStatement();
         ResultSet res = req.executeQuery("SELECT * FROM RESUMEPARTIE");
         BigInteger Val = BigInteger.valueOf(valeur);
@@ -94,9 +94,9 @@ public class FctResumePartie {
     }
         
         //Moyenne totale d'une valeur de dès
-    public double moyenne_Lance_De(int valeur) throws SQLException{
+    public double total_Moyenne_Lance_De(int valeur) throws SQLException{
         double total = new Double(total_Lance());
-        double nbLance = new Double(total_Lance_De(valeur));
+        double nbLance = new Double(total_Nb_Lance_De(valeur));
         return (nbLance/total);
     }
     
@@ -104,27 +104,74 @@ public class FctResumePartie {
     
         //Nombre de lancé de dès
     public int lance(BigInteger Code_Partie) throws SQLException{
-        int numLance = 0;
         PreparedStatement reqSelectParam = connection.prepareStatement("SELECT * FROM RESUMEPARTIE WHERE CODE_PARTIE = ?");
         reqSelectParam.setInt(1, Code_Partie.intValue());
         ResultSet res = reqSelectParam.executeQuery();
+        int nbLance = 0;
         while(res.next()){
-            numLance = res.getRow();
+            nbLance = res.getRow();
         }
-        return numLance;
+        return nbLance;
     }
     
         //Valeurs des dès tombées
+    //fct de calcul du score à faire avant
     
-        //Moyenne des valeurs des dès 
+        //Moyenne des valeurs des dès
+    //fct de calcul du score à faire avant
     
         //Nombre de fois qu'une valeur de dès précise est tombée
+    public int nb_Lance_De(BigInteger Code_Partie, int valeur) throws SQLException{
+        PreparedStatement reqSelectParam = connection.prepareStatement("SELECT * FROM RESUMEPARTIE WHERE CODE_PARTIE = ?");
+        reqSelectParam.setInt(1, Code_Partie.intValue());
+        ResultSet res = reqSelectParam.executeQuery();
+        //BigInteger Val = BigInteger.valueOf(valeur);
+        
+        int nbLance = 0;
+        while(res.next()){
+            if(res.getInt("DES_1") == valeur){
+                nbLance += 1;
+            }
+            if(res.getInt("DES_2") == valeur){
+                nbLance += 1;
+            }
+            if(res.getInt("DES_3") == valeur){
+                nbLance += 1;
+            }
+        }
+        return nbLance;
+    }
     
         //Moyenne d'une valeur de dès
+    public double moyenne_Lance_De(BigInteger Code_Partie, int valeur) throws SQLException{
+        double total = new Double(lance(Code_Partie));
+        double nbLance = new Double(nb_Lance_De(Code_Partie, valeur));
+        return (nbLance/total);
+    }
     
         //3 valeurs du dernier lancé de dès
+    public int[] derniers_Des(BigInteger Code_Partie) throws SQLException{
+        PreparedStatement reqSelectParam = connection.prepareStatement("SELECT * FROM RESUMEPARTIE WHERE CODE_PARTIE = ?");
+        reqSelectParam.setInt(1, Code_Partie.intValue());
+        ResultSet res = reqSelectParam.executeQuery();
+        res.last();
+        int [] des = {res.getInt("DES_1"),res.getInt("DES_2"),res.getInt("DES_3")};
+        return des;
+    }
     
     //MISE A JOUR
     
         //Maj de Des_1, Des_2 et Des_3 lors de la fin du tout d'un joueur
+    public void maj_Des(BigInteger Code_Partie, int [] des) throws SQLException{
+        int nbLance = lance(Code_Partie);
+        int nb = 0;
+        PreparedStatement reqUpdateParam = connection.prepareStatement("UPDATE RESUMEPARTIE SET DES_1 = ?, DES_2 = ?, DES_3 = ? WHERE CODE_PARTIE = ? AND NUM_LANCE_DES = ?");
+        reqUpdateParam.setInt(1,des[0]);
+        reqUpdateParam.setInt(2,des[1]);
+        reqUpdateParam.setInt(3,des[2]);
+        reqUpdateParam.setInt(4, Code_Partie.intValue());
+        reqUpdateParam.setInt(5, nbLance);
+        nb = reqUpdateParam.executeUpdate();
+        System.out.println(nb + " ligne ont été update");
+    }
 }
