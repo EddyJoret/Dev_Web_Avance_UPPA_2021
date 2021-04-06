2ae8706ea49b72dad0cee5ec40673c8976282c87
EddyJoret <eddyjoret@hotmail.fr>
2021-04-06 16:26:33 +0200

    Update Projet_CulDeChouette_2021.java

--- Projet_CulDeChouette_2021/src/projet_culdechouette_2021/Projet_CulDeChouette_2021.java
+++ Projet_CulDeChouette_2021/src/projet_culdechouette_2021/Projet_CulDeChouette_2021.java
@@ -1,5 +1,10 @@
 package projet_culdechouette_2021;
 
+import FctPojo.FctJoueur;
+import FctPojo.FctPartie;
+import FctPojo.FctResumePartie;
+import FctPojo.FctScorePartie;
+import FctPojo.FctStatistique;
 import java.math.BigDecimal;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,6 +15,13 @@ public class Projet_CulDeChouette_2021 {
     /**
      * @param args the command line arguments
      */
+    
+    FctJoueur Joueur;
+    FctPartie Partie;
+    FctResumePartie Resume;
+    FctScorePartie Score;
+    FctStatistique Stat;
+    
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("Projet_CulDeChouette_2021PU");
         EntityManager em = emf.createEntityManager();
